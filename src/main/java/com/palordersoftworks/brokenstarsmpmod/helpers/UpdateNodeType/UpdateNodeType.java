1d6c0e6a0c58d3d7a13c4a3c210db09c15137dd4
User <User@DESKTOP-60SVC0H>
2026-04-09 17:37:36 +0200

    Fixed critical redstone issues as it takes 1 tick more for redstone to respond

--- src/main/java/com/palordersoftworks/brokenstarsmpmod/helpers/UpdateNodeType/UpdateNodeType.java
+++
@@ -1,9 +0,0 @@
-package com.palordersoftworks.brokenstarsmpmod.helpers.UpdateNodeType;
-
-public class UpdateNodeType {
-    public enum UpdateNodeTypes {
-        UNKNOWN,
-        REDSTONE,
-        OTHER;
-    }
-}
