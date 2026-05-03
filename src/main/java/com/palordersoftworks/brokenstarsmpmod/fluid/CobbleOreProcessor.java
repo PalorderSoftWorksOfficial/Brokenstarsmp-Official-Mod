39ca6feefedc3260c4cbea0a91159da0ddb89df7
User <User@DESKTOP-60SVC0H>
2026-05-03 13:36:48 +0200

    Added more management stuff and made it so the fabric-1.21.10 actually has source code and is synced with the fabric 1.21.11 version and im writing source code for paper or spigot plugins lol anyways some rules have NOT been fully coded please be aware of this and removed essentialcommands mixin

--- src/main/java/com/palordersoftworks/brokenstarsmpmod/fluid/CobbleOreProcessor.java
+++ src/main/java/com/palordersoftworks/brokenstarsmpmod/fluid/CobbleOreProcessor.java
@@ -20,8 +20,17 @@ public final class CobbleOreProcessor {
             Blocks.LAPIS_ORE,
             Blocks.DIAMOND_ORE,
             Blocks.EMERALD_ORE,
+            Blocks.DEEPSLATE_COAL_ORE,
+            Blocks.DEEPSLATE_COPPER_ORE,
+            Blocks.DEEPSLATE_IRON_ORE,
+            Blocks.DEEPSLATE_GOLD_ORE,
+            Blocks.DEEPSLATE_REDSTONE_ORE,
+            Blocks.DEEPSLATE_LAPIS_ORE,
+            Blocks.DEEPSLATE_DIAMOND_ORE,
+            Blocks.DEEPSLATE_EMERALD_ORE,
             Blocks.NETHER_QUARTZ_ORE,
-            Blocks.NETHER_GOLD_ORE
+            Blocks.NETHER_GOLD_ORE,
+            Blocks.ANCIENT_DEBRIS
     };
 
     public static void process(net.minecraft.world.World world, BlockPos pos) {
