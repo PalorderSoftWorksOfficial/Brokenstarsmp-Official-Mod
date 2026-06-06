bd730ce5c4433f7f53e20cdea3c5e584775d060a
PalorderSoftWorksOfficial <palordersoftworks@gmail.com>
2026-06-06 20:26:13 +0200

    Add /immortal toggle command

---
+++ src/main/java/com/palordersoftworks/brokenstarsmpmod/commands/ImmortalCommand.java
@@ -0,0 +1,35 @@
+package com.palordersoftworks.brokenstarsmpmod.commands;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.palordersoftworks.brokenstarsmpmod.config.UnstableSMPRules;
+import com.palordersoftworks.brokenstarsmpmod.unstablesmp.UnstableSMPFeatures;
+import net.minecraft.command.CommandRegistryAccess;
+import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+
+import static net.minecraft.server.command.CommandManager.argument;
+import static net.minecraft.server.command.CommandManager.literal;
+
+public final class ImmortalCommand {
+    private ImmortalCommand() {
+    }
+
+    public static void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess registryAccess, CommandManager.RegistrationEnvironment environment) {
+        dispatcher.register(literal("immortal")
+                .requires(source -> UnstableSMPRules.IMMORTAL_SYSTEM_ENABLED && PermissionUtil.hasImmortalPermission(source))
+                .then(argument("player", EntityArgumentType.player())
+                        .executes(context -> {
+                            ServerPlayerEntity target = EntityArgumentType.getPlayer(context, "player");
+                            boolean enabled = !UnstableSMPFeatures.isImmortal(target);
+                            UnstableSMPFeatures.setImmortal(target, enabled);
+
+                            context.getSource().sendFeedback(() -> Text.literal(target.getName().getString() + " immortal set to " + enabled).formatted(Formatting.YELLOW), false);
+                            target.sendMessage(Text.literal(enabled ? "Immortality enabled" : "Immortality disabled").formatted(Formatting.AQUA), false);
+                            return 1;
+                        })));
+    }
+}
