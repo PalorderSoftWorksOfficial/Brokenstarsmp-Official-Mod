14f927d294c08c6401ac30f9232d9a5c1e26050a
PalorderSoftWorksOfficial <palordersoftworks@gmail.com>
2026-06-06 20:26:56 +0200

    Use packet message for immortal toggle

--- src/main/java/com/palordersoftworks/brokenstarsmpmod/commands/ImmortalCommand.java
+++ src/main/java/com/palordersoftworks/brokenstarsmpmod/commands/ImmortalCommand.java
@@ -5,6 +5,7 @@ import com.palordersoftworks.brokenstarsmpmod.config.UnstableSMPRules;
 import com.palordersoftworks.brokenstarsmpmod.unstablesmp.UnstableSMPFeatures;
 import net.minecraft.command.CommandRegistryAccess;
 import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -28,7 +29,7 @@ public final class ImmortalCommand {
                             UnstableSMPFeatures.setImmortal(target, enabled);
 
                             context.getSource().sendFeedback(() -> Text.literal(target.getName().getString() + " immortal set to " + enabled).formatted(Formatting.YELLOW), false);
-                            target.sendMessage(Text.literal(enabled ? "Immortality enabled" : "Immortality disabled").formatted(Formatting.AQUA), false);
+                            target.networkHandler.sendPacket(new GameMessageS2CPacket(Text.literal(enabled ? "Immortality enabled" : "Immortality disabled").formatted(Formatting.AQUA), false));
                             return 1;
                         })));
     }
