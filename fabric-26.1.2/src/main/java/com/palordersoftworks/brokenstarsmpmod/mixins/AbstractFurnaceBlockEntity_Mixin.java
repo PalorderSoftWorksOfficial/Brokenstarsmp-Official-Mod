b33fb0506e3e555a894dcf549414536f045ff344
PalorderSoftWorksOfficial <palordersoftworks@gmail.com>
2026-08-19 12:30:34 +0200

    Fix 26.x furnace cooking-speed mixin signature

--- fabric-26.1.2/src/main/java/com/palordersoftworks/brokenstarsmpmod/mixins/AbstractFurnaceBlockEntity_Mixin.java
+++ fabric-26.1.2/src/main/java/com/palordersoftworks/brokenstarsmpmod/mixins/AbstractFurnaceBlockEntity_Mixin.java
@@ -1,19 +1,19 @@
 package com.palordersoftworks.brokenstarsmpmod.mixins;
 
-import net.minecraft.block.entity.AbstractFurnaceBlockEntity;
+import com.palordersoftworks.brokenstarsmpmod.config.ServerRules;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import static com.palordersoftworks.brokenstarsmpmod.config.ServerRules.FURNACE_COOKING_SPEED;
-
 @Mixin(AbstractFurnaceBlockEntity.class)
 public class AbstractFurnaceBlockEntity_Mixin {
 
-    @Inject(method = "getCookTime", at = @At("HEAD"), cancellable = true)
-    private static void brokenstarsmpmod$configurableCookingSpeed(CallbackInfoReturnable<Integer> cir) {
-        int speed = FURNACE_COOKING_SPEED;
+    @Inject(method = "getTotalCookTime", at = @At("HEAD"), cancellable = true)
+    private static void brokenstarsmpmod$configurableCookingSpeed(ServerLevel level, AbstractFurnaceBlockEntity furnace, CallbackInfoReturnable<Integer> cir) {
+        int speed = ServerRules.FURNACE_COOKING_SPEED;
         if (speed <= 0) speed = 1;
         cir.setReturnValue(speed);
     }
