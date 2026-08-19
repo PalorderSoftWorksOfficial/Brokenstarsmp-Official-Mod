b2f30974568e9a66db65ab1fa5047dc80a9d6375
PalorderSoftWorksOfficial <palordersoftworks@gmail.com>
2026-08-19 12:32:22 +0200

    Port fake player invulnerability mixin to Mojang mappings

--- fabric-26.1.2/src/main/java/com/palordersoftworks/brokenstarsmpmod/mixins/ServerPlayerEntity_Mixin.java
+++ fabric-26.1.2/src/main/java/com/palordersoftworks/brokenstarsmpmod/mixins/ServerPlayerEntity_Mixin.java
@@ -1,19 +1,17 @@
 package com.palordersoftworks.brokenstarsmpmod.mixins;
 
-import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.damagesource.DamageSource;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ServerPlayerEntity.class)
+@Mixin(ServerPlayer.class)
 public abstract class ServerPlayerEntity_Mixin {
     @Inject(method = "isInvulnerableTo", at = @At("HEAD"), cancellable = true)
-    private void removeFakePlayerInvulnerability(ServerWorld world, DamageSource source, CallbackInfoReturnable<Boolean> cir) {
+    private void removeFakePlayerInvulnerability(ServerLevel level, DamageSource source, CallbackInfoReturnable<Boolean> cir) {
         try {
             Class<?> fakeClass = Class.forName("carpet.patches.EntityPlayerMPFake");
             if (fakeClass.isInstance(this)) {
